0a9fc1759adbfb4ca6d7855cce654219f1a22be0
Aafreen Parveen <aafreenparveen2575@gmail.com>
2026-03-05 21:54:34 +0530

    third commit

--- petpals-sanity/sanity.config.js
+++ petpals-sanity/sanity.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   name: 'default',
   title: 'petpals-sanity',
 
-  projectId: '8cc6p2y9',
-  dataset: 'production',
+  projectId: process.env.VITE_SANITY_PROJECT_ID,
+  dataset: process.env.VITE_SANITY_DATASET,
 
   plugins: [structureTool(), visionTool()],
 
